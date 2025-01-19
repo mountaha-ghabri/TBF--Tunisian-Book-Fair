e9fb8d9a4698bce3c92e21bcf0fcceca2057bf07
Mountaha <moontahaghabry@gmail.com>
2025-01-19 12:52:10 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,24 +1,22 @@
-const express = require("express");
-const app = express();
-const port = 8080;
+const express = require('express');  
+const app = express();  
 
-// Middleware
-app.use(express.json());
+app.use(express.json());  
 
-// Import Routes
-const booksRoutes = require("./books");
-const ordersRoutes = require("./orders");
+// Import Routes  
+const booksRoutes = require('./books');  
+const ordersRoutes = require('./orders');  
 
-// Routes
-app.use("/books", booksRoutes);  // Route for fetching books data
-app.use("/orders", ordersRoutes); // Route for posting orders
+// Use Routes  
+app.use('/books', booksRoutes);  // Route for fetching books data  
+app.use('/orders', ordersRoutes); // Route for posting orders  
 
-// Default route
-app.get("/", (req, res) => {
-    res.send("Welcome to the Book Fair API!");
-});
+// Default route  
+app.get('/', (req, res) => {  
+    res.send('Welcome to the Book Fair API!');  
+});  
 
-// Start the server
-app.listen(8080, '0.0.0.0', () => {
-    console.log('Server is running on port 8080');
-});
+// Export the express app as a serverless function  
+module.exports = (req, res) => {  
+    app(req, res);  
+};
