4e4785d4bd3bcdea1fa73da4828cd0a08df24ba1
Mountaha <moontahaghabry@gmail.com>
2025-01-19 12:39:22 +0100

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+// Middleware
+app.use(express.json());
+
+// Import Routes
+const booksRoutes = require("./routes/books");
+const ordersRoutes = require("./routes/orders");
+
+// Routes
+app.use("/books", booksRoutes);  // Route for fetching books data
+app.use("/orders", ordersRoutes); // Route for posting orders
+
+// Default route
+app.get("/", (req, res) => {
+    res.send("Welcome to the Book Fair API!");
+});
+
+// Start the server
+app.listen(8080, '0.0.0.0', () => {
+    console.log('Server is running on port 8080');
+});
